744c373de165deb8f04509eff8a92ada27bb7e98
Fess292 <74976243+Fess292@users.noreply.github.com>
2021-02-28 22:26:07 +0300

    портфолио 1.0

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const h5 = document.querySelectorAll('h5'),
+    lines = document.querySelectorAll('.work__skills-points span');
+
+h5.forEach((item,i) => {
+    lines [i].style.width = item.innerHTML;
+
+ 
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+
+});
