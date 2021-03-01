c76b9160b5209e297f12e17ddd9dbaaa16bb69aa
Fess292 <74976243+Fess292@users.noreply.github.com>
2021-03-01 15:59:37 +0300

    phpmailer

--- src/mailer/send.php
+++ src/mailer/send.php
@@ -8,9 +8,10 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $text = $_POST['text'];
+$file = $_FILES['myfile'];
 
 // Формирование самого письма
-$title = "Заголовок письма";
+$title = "Из резюме";
 $body = "
 <h2>Новое письмо</h2>
 <b>Имя:</b> $name<br>
@@ -28,18 +29,30 @@ try {
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Host       = 'SMTP.Office365.com'; // SMTP сервера вашей почты
     $mail->Username   = 'fess292'; // Логин на почте
-    $mail->Password   = 'integraR292'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('fess292@gmail.com', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+    $mail->Password   = "poiu'lkj"; // Пароль на почте
+    $mail->SMTPSecure = 'STARTTLS';
+    $mail->Port       = 587;
+    $mail->setFrom('fess292@outlook.com', 'Fess292'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
     $mail->addAddress('fess292@outlook.com');  
-    $mail->addAddress('gsx292@yandex.ru'); // Ещё один, если нужен
+    $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
 
-
+    // Прикрипление файлов к письму
+if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
+        } else {
+            $rfile[] = "Не удалось прикрепить файл $filename";
+        }
+    }   
+}
 // Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
